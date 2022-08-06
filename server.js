7f5a965f26ac461d1558b5bf54be839399dd8c52
ashachakre0906 <chourpagar.asha@gmail.com>
2022-08-05 18:03:30 -0700

    added folder structure

---
+++ server.js
@@ -0,0 +1,41 @@
+require("dotenv").config();
+const express = require("express");
+const exphbs = require("express-handlebars");
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+const sequelize = require("./config/connection");
+const routes = require("./routes");
+
+const hbs = exphbs.create({});
+
+const sessionSettings = {
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+// Template Engine Setup: sets up express with handlebars
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+// middlewares
+app.use(express.static("public"));
+app.use(session(sessionSettings));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  server.listen(PORT, () =>
+    console.log(`Listening on PORT: ${PORT} Yass mama!`)
+  );
+});
