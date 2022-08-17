63e33bb27b6823537880930f8e230fbf0e4633f3
ashachakre0906 <chourpagar.asha@gmail.com>
2022-08-17 16:08:12 -0700

    added get route in dashboard.routes

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -1,23 +1,24 @@
-const router = require('express').Router();
-const {Post , User}= require('../models');
-const withAuth = require('../util/auth');
+const router = require("express").Router();
+const { Post, User } = require("../models");
+const withAuth = require("../util/auth");
 
 //Get All Posts
 
-router.get('/' ,withAuth, async (req ,res) => {
-    try {
-        const getPost = await Post.findAll(
-            {
-        where:{ 'userId'
+router.get("/", withAuth, async (req, res) => {
+  try {
+    const getPost = await Post.findAll({
+      where: { user_id: req.session.user_id },
+      include: [User],
+    });
+    const posts = getPost.map((post) => post.get({ plain: true }));
+    console.log(posts);
+    res.render("all-posts", {
+      layout: "dasboard",
+      posts,
+    });
+  } catch (err) {
+    res.redirect("login");
+  }
+});
 
-    })
-    }
-
-
-
-})
-
-
-
-
-module.exports = router
+module.exports = router;
