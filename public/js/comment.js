914188281da0a328e0b1bb6df7a21db6c07d99e2
ashachakre0906 <chourpagar.asha@gmail.com>
2022-08-22 23:46:55 -0700

    added comment.js FE and fetch the api url from backend route and added a event listener to the button

---
+++ public/js/comment.js
@@ -0,0 +1,23 @@
+const postId = document.querySelector('input[name="post-id"]').value.trim();
+const commentFormHandler = async (event) => {
+  event.preventDefault();
+  const commentBody = document
+    .querySelector('textarea[name="comment-body"]')
+    .value.trim();
+  if (commentBody) {
+    const response = await fetch("/api/comment", {
+      method: "POST",
+      body: JSON.stringify({ postId, commentBody }),
+      headers: { "Content-type": "application/json" },
+    });
+    if (response.ok) {
+      document.location.reload();
+    } else {
+      alert(response.status.text);
+    }
+  }
+};
+
+document
+.querySelector('#new-comment-form')
+.addEventListener('submit',commentFormHandler);
